1b9aaf5bfa176cd876e512f7713433e5c784a3be
alinecbsr <alinecbsr@gmail.com>
2020-10-12 20:36:41 -0300

    Implemented mapbox

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { Link } from "react-router-dom";
+import { FiPlus } from "react-icons/fi";
+import { Map, TileLayer } from "react-leaflet";
 
-import 'leaflet/dist/leaflet.css';
+import "leaflet/dist/leaflet.css";
 
 import mapMarkerImg from "../images/mapMarker.svg";
 
-import '../styles/pages/orphanages-map.css';
+import "../styles/pages/orphanages-map.css";
 
 function OrphanagesMap() {
   return (
@@ -23,15 +23,18 @@ function OrphanagesMap() {
           <span>Pernambuco</span>
         </footer>
       </aside>
-      <Map 
-        center={[-8.042177,-35.0086755]}
-        zoom={15}
-        style={{ width: "100%", height: "100%"}}
+      <Map
+        center={[-8.0711378,-34.9599236]}
+        zoom={12.3}
+        style={{ width: "100%", height: "100%" }}
       >
-        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
       </Map>
       <Link to="" className="create-orphanage">
-        <FiPlus size={32} color="#fff"/>
+        <FiPlus size={32} color="#fff" />
       </Link>
     </div>
   );
