319f89eafbc50a846eb8f3d546bfffc82bc84c45
alinecbsr <alinecbsr@gmail.com>
2020-10-15 15:46:33 -0300

    Upload images with DB registration

--- server/src/models/Orphanage.ts
+++ server/src/models/Orphanage.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany, JoinColumn } from 'typeorm';
+import Image from './Image';
 
 @Entity('orphanages')
 export default class Orphanage {
@@ -6,7 +7,7 @@ export default class Orphanage {
   id: number;
 
   @Column()
-  name: string;
+  name: string; 
 
   @Column()
   latitude: number;
@@ -25,4 +26,10 @@ export default class Orphanage {
 
   @Column()
   open_on_weekends: boolean;
+
+  @OneToMany(() => Image, image => image.orphanage, {
+    cascade: ['insert', 'update']
+  })
+  @JoinColumn({ name: 'orphanage_id'})
+  images: Image[];
 }
